4c215ad8a571b419f7372a10140e1c55124f713f
toan Nguyen <toannq0988@gmail.com>
2018-07-28 15:57:03 +0700

    paging With Storeprocedure

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PagingWithStore._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField HeaderText="CustomerId" DataField="CustomerId" />
+                <asp:BoundField HeaderText="ContactName" DataField="ContactName" />
+                <asp:BoundField HeaderText="CompanyName" DataField="CompanyName" />
+            </Columns>
+        </asp:GridView>
+        <br />
+        PageSize:
+        <asp:DropDownList ID="ddlPageSize" runat="server" AutoPostBack="true" OnSelectedIndexChanged="PageSize_Changed">
+            <asp:ListItem Text="10" Value="10" />
+            <asp:ListItem Text="25" Value="25" />
+            <asp:ListItem Text="50" Value="50" />
+        </asp:DropDownList>
+        <hr />
+        <asp:Repeater ID="rptPager" runat="server">
+            <ItemTemplate>
+                <asp:LinkButton ID="lnkPage" runat="server" Text='<%#Eval("Text") %>' CommandArgument='<%# Eval("Value") %>' Enabled='<%# Eval("Enabled") %>' OnClick="Page_Changed" CssClass='<%#Convert.ToBoolean(Eval("Enabled")) ? "ActiveLinkBtn" :"InActiveLinkBtn"%>'></asp:LinkButton>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PagingWithStore
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetCustomersPageWise(1);
+            }
+        }
+
+        private void GetCustomersPageWise(int pageIndex)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string constring = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constring))
+                {
+                    using (SqlCommand cmd = new SqlCommand("PagingTest", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@PageIndex", pageIndex);
+                        cmd.Parameters.AddWithValue("@PageSize", int.Parse(ddlPageSize.SelectedValue));
+                        cmd.Parameters.Add("@RecordCount", SqlDbType.Int, 4);
+                        cmd.Parameters["@RecordCount"].Direction = ParameterDirection.Output;
+                        con.Open();
+                        using (var da = new SqlDataAdapter(cmd))
+                        {
+                            da.Fill(dt);
+                        }
+                        IDataReader idr = cmd.ExecuteReader();
+                        BindingDataSourceIntoGridView(idr);
+                        con.Close();
+                        int recordCount = Convert.ToInt32(cmd.Parameters["@RecordCount"].Value);
+                        this.PopulatePager(recordCount, pageIndex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Bindings the data source into grid view.
+        /// </summary>
+        /// <param name="idr">Source IDataReader.</param>
+        private void BindingDataSourceIntoGridView(IDataReader idr)
+        {
+            GridView1.DataSource = idr;
+            GridView1.DataBind();
+            idr.Close();
+        }
+
+        private void PopulatePager(int recordCount, int currentPage)
+        {
+            double dblPageCount = (double)((decimal)recordCount / decimal.Parse(ddlPageSize.SelectedValue));
+            int pageCount = (int)Math.Ceiling(dblPageCount);
+            List<ListItem> pages = new List<ListItem>();
+            if (pageCount > 0)
+            {
+                pages.Add(new ListItem("First", "1", currentPage > 1));
+                for (int i = 1; i <= pageCount; i++)
+                {
+                    pages.Add(new ListItem(i.ToString(), i.ToString(), i != currentPage));
+                }
+                pages.Add(new ListItem("Last", pageCount.ToString(), currentPage < pageCount));
+            }
+            rptPager.DataSource = pages;
+            rptPager.DataBind();
+        }
+
+        protected void PageSize_Changed(object sender, EventArgs e)
+        {
+            this.GetCustomersPageWise(1);
+        }
+
+        protected void Page_Changed(object sender, EventArgs e)
+        {
+            int pageIndex = int.Parse((sender as LinkButton).CommandArgument);
+            this.GetCustomersPageWise(pageIndex);
+        }
+    }
+}
